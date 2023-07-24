c6a8a77a9974467105bf087a2d62a4fce48f7c15
sim-sal <simone.saladino93@gmail.com>
2023-07-24 20:06:26 +0200

    Create MIGRATION, MODEL, FACTORY and SEEDER

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -5,17 +5,17 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Project;
+
+class ProjectTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            ProjectTableSeeder::class
-        ]);
+        Project::factory()->count(10)->create();
     }
 }
